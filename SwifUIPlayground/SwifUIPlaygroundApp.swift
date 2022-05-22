e9d428f79c0a874444852a0746a5c1fed05f82e7
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-05-22 16:44:20 +0900

    [FEAT] : 프로젝트 시작

---
+++ SwifUIPlayground/SwifUIPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwifUIPlaygroundApp.swift
+//  SwifUIPlayground
+//
+//  Created by geonhyeong on 2022/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwifUIPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

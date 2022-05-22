7be1d7bdb0866eb3260a67a40980e9066bff6fa5
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-05-22 17:44:20 +0900

    [FEAT] : TabBar 구성

--- SwifUIPlayground/Sources/View/ContentView.swift
+++ SwifUIPlayground/Sources/View/ContentView.swift
@@ -8,8 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection: Tab = .home
+    
+    enum Tab {
+        case home
+        case calendar
+        case album
+        case setting
+    }
+    
     var body: some View {
-        MyView(customFont: .title)
+        TabView(selection: $selection) {
+            AssetView()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("홈")
+                }
+                .tag(Tab.home)
+            Color.green
+//                .edgesIgnoringSafeArea(.all)
+                .tabItem {
+                    Image(systemName: "calendar")
+                    Text("일정")
+                }
+                .tag(Tab.calendar)
+            Color.red
+//                .edgesIgnoringSafeArea(.all)
+                .tabItem {
+                    Image(systemName: "photo.on.rectangle.angled")
+                    Text("엘범")
+                }
+                .tag(Tab.album)
+            Color.yellow
+//                .edgesIgnoringSafeArea(.all)
+                .tabItem {
+                    Image(systemName: "gearshape.fill")
+                    Text("설정")
+                }
+                .tag(Tab.setting)
+        }
     }
 }
 
